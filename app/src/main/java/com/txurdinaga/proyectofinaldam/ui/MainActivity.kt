28d2f6d3b49fd1d382bc73460cf476bf2def834b
JdanielRo <josedanieltxurdinaga@gmail.com>
2024-02-19 11:56:54 +0100

    main en ui

--- app/src/main/java/com/txurdinaga/proyectofinaldam/ui/MainActivity.kt
+++ app/src/main/java/com/txurdinaga/proyectofinaldam/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.txurdinaga.proyectofinaldam
+package com.txurdinaga.proyectofinaldam.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.txurdinaga.proyectofinaldam.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
