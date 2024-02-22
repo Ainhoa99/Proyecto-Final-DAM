1a29a78e56d56a0b300d3f361c294635e5443b78
temporizadorMikel <mikellopez010903@gmail.com>
2024-02-22 12:55:53 +0100

    diseño de la pagina principal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
     testImplementation("junit:junit:4.13.2")
+    implementation ("com.google.android.material:material:1.2.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
@@ -54,5 +56,6 @@ dependencies {
     val navVersion = "2.7.5"
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+
 
 }
--- app/src/main/java/com/txurdinaga/proyectofinaldam/ui/MainActivity.kt
+++ app/src/main/java/com/txurdinaga/proyectofinaldam/ui/MainActivity.kt
@@ -2,16 +2,71 @@ package com.txurdinaga.proyectofinaldam.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.txurdinaga.proyectofinaldam.R
 import com.txurdinaga.proyectofinaldam.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        drawerLayout = binding.drawerLayout
+        initNavigation()
+
+
+        supportActionBar?.title = "Unkina SBT"
     }
+
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHost.navController
+        // Aquí especificamos los ID de los destinos de nivel superior, excluyendo el ID del destino de inicio
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.principalFragment,
+                R.id.gestionEquiposFragment,
+                R.id.inscripcionFragment,
+                R.id.gestionPartidosFragment,
+                R.id.colaboradoresFragment2,
+                R.id.informacionFragment2
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.navView.setupWithNavController(navController)
+
+        // Escuchar cambios en los destinos del NavController
+        navController.addOnDestinationChangedListener { controller, destination, _ ->
+            if (destination.id == R.id.principalFragment ||
+                destination.id == R.id.gestionEquiposFragment ||
+                destination.id == R.id.inscripcionFragment ||
+                destination.id == R.id.gestionPartidosFragment ||
+                destination.id == R.id.informacionFragment2 ||
+                destination.id == R.id.colaboradoresFragment2
+            ) {
+                supportActionBar?.title = "Unkina SBT" // Establecer el título deseado para los destinos específicos
+            }
+        }
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navHostFragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
 }
