958a20d55b7c03401c64885803547797c2efa404
Diego Navarro <diegoonavarro2002@gmail.com>
2024-02-22 10:32:22 +0100

    Diseño

--- app/src/main/java/com/txurdinaga/proyectofinaldam/ui/PruebaFragment.kt
+++ app/src/main/java/com/txurdinaga/proyectofinaldam/ui/PruebaFragment.kt
@@ -6,19 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.txurdinaga.proyectofinaldam.R
-import com.txurdinaga.proyectofinaldam.databinding.FragmentAjustesBinding
 import com.txurdinaga.proyectofinaldam.databinding.FragmentCalendarioBinding
 
 
-class AjustesFragment : Fragment() {
-
-    private var _binding: FragmentAjustesBinding? = null
+class PruebaFragment : Fragment() {
+    private var _binding: FragmentCalendarioBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentAjustesBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentCalendarioBinding.inflate(layoutInflater, container, false)
 
         return binding.root
     }
