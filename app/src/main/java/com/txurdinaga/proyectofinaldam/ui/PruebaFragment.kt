748f2d8c39fffbd1729a576c5430b2cda5a87617
Diego Navarro <diegoonavarro2002@gmail.com>
2024-02-26 11:49:09 +0100

    diseño y control de vacios
    Contacto y Prueba

--- app/src/main/java/com/txurdinaga/proyectofinaldam/ui/PruebaFragment.kt
+++ app/src/main/java/com/txurdinaga/proyectofinaldam/ui/PruebaFragment.kt
@@ -1,6 +1,7 @@
 package com.txurdinaga.proyectofinaldam.ui
 
 import android.app.DatePickerDialog
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.txurdinaga.proyectofinaldam.R
 import com.txurdinaga.proyectofinaldam.databinding.FragmentCalendarioBinding
 import com.txurdinaga.proyectofinaldam.databinding.FragmentPruebaBinding
@@ -21,6 +23,7 @@ class PruebaFragment : Fragment() {
     private lateinit var textViewDate: TextView
     private lateinit var buttonDatePicker: Button
     private val calendar: Calendar = Calendar.getInstance()
+    private var allFieldsFilled : Boolean = false
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,6 +35,43 @@ class PruebaFragment : Fragment() {
         textViewDate.setOnClickListener {
             showDatePicker()
         }
+
+        var boton = binding.buttonEnviar
+
+        boton.setOnClickListener {
+
+            allFieldsFilled = true
+
+            if (binding.tlfn.text.toString().trim().isEmpty()) {
+                binding.tlfnLayout.error = "Escribe un numero de teléfono"
+                binding.tlfnLayout.requestFocus()
+                allFieldsFilled = false
+            }
+
+            if (binding.nombre.text.toString().trim().isEmpty()) {
+                binding.nombreLayout.error = "Escribe un nombre"
+                binding.nombreLayout.requestFocus()
+                allFieldsFilled = false
+            }
+
+            if (binding.textInputEditTextDate.text.toString().trim().isEmpty()) {
+                binding.textInputLayoutDate.error = "Elige una fecha de nacimiento"
+                binding.textInputLayoutDate.requestFocus()
+                allFieldsFilled = false
+            }
+
+            if (binding.email.text.toString().trim().isEmpty()) {
+                binding.emailLayout.error = "Escribe un e-mail"
+                binding.emailLayout.requestFocus()
+                allFieldsFilled = false
+            }
+
+            if(allFieldsFilled === true){
+                //sendEmail()
+            }
+
+        }
+
         return binding.root
     }
 
@@ -47,5 +87,37 @@ class PruebaFragment : Fragment() {
             }, year, month, dayOfMonth)
 
         datePickerDialog.show()
+
     }
+
+    /*private fun sendEmail() {
+        val nombre = binding.nombre.text.toString().trim()
+        val fecha = binding.textInputEditTextDate.text.toString().trim()
+        val tlfn = binding.tlfn.text.toString().trim()
+        val correo = "2grupotxurdinaga@gmail.com"
+        val asunto = "UN JUGADOR/A QUIERE REALIZAR UNA PRUEBA"
+        val mensaje = "Nombre:  $nombre \n" +
+                      "Fecha de Nacimiento:  $fecha \n" +
+                      "Número de Teléfono:  $tlfn \n"
+
+        val senderEmail = binding.email.text.toString().trim()
+
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(correo))
+        intent.putExtra(Intent.EXTRA_SUBJECT, asunto)
+        intent.putExtra(Intent.EXTRA_TEXT, mensaje)
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(senderEmail))
+
+        intent.type = "message/rfc822"
+
+        try {
+            startActivity(Intent.createChooser(intent, "Selecciona una aplicación de correo"))
+        } catch (ex: android.content.ActivityNotFoundException) {
+            // Maneja la excepción si no hay aplicaciones de correo electrónico disponibles
+            Toast.makeText(requireContext(), "No se encontró ninguna aplicación de correo electrónico instalada.", Toast.LENGTH_SHORT).show()
+        } catch (ex: Exception) {
+            // Maneja otras excepciones que puedan ocurrir
+            Toast.makeText(requireContext(), "Error al enviar el correo electrónico.", Toast.LENGTH_SHORT).show()
+        }
+    }*/
 }
