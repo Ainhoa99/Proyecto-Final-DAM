21c26289e1737e5d5f8d20e2b0238a13887cb255
tortilla-seca <tortilla-seca.u0ves@simplelogin.com>
2024-02-27 01:20:44 +0100

    user repo WIP

---
+++ app/src/main/java/com/txurdinaga/proyectofinaldam/data/model/User.kt
@@ -0,0 +1,70 @@
+package com.txurdinaga.proyectofinaldam.data.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class User(
+    val token: String? = null, // TODO Almacenar el token junto al usuario o siempre fuera?
+    val email: String? = null,
+    val password: String? = null,
+    val name: String? = null,
+    val surname: String? = null,
+    val picture: String? = null,
+    val isAdmin: Boolean? = null,
+    val isFirstLogin: Boolean? = null,
+    val lastSeen: Long? = null,
+    val dateOfBirth: Long? = null,
+    val teamId: Int? = null,
+    val roleId: Int? = null,
+    val familyId: String? = null,
+    val isActive: Boolean? = null
+)
+
+class UserBuilder { // Builder Pattern
+    var token: String? = null
+    var email: String? = null
+    var password: String? = null
+    var name: String? = null
+    var surname: String? = null
+    var picture: String? = null
+    var isAdmin: Boolean? = null
+    var isFirstLogin: Boolean? = null
+    var lastSeen: Long? = null
+    var dateOfBirth: Long? = null
+    var teamId: Int? = null
+    var roleId: Int? = null
+    var familyId: String? = null
+    var isActive: Boolean? = null
+
+    fun token(token: String?) = apply { this.token = token }
+    fun email(email: String?) = apply { this.email = email }
+    fun password(password: String?) = apply { this.password = password }
+    fun name(name: String?) = apply { this.name = name }
+    fun surname(surname: String?) = apply { this.surname = surname }
+    fun picture(picture: String?) = apply { this.picture = picture }
+    fun isAdmin(isAdmin: Boolean?) = apply { this.isAdmin = isAdmin }
+    fun isFirstLogin(isFirstLogin: Boolean?) = apply { this.isFirstLogin = isFirstLogin }
+    fun lastSeen(lastSeen: Long?) = apply { this.lastSeen = lastSeen }
+    fun dateOfBirth(dateOfBirth: Long?) = apply { this.dateOfBirth = dateOfBirth }
+    fun teamId(teamId: Int?) = apply { this.teamId = teamId }
+    fun roleId(roleId: Int?) = apply { this.roleId = roleId }
+    fun familyId(familyId: String?) = apply { this.familyId = familyId }
+    fun isActive(isActive: Boolean?) = apply { this.isActive = isActive }
+
+    fun build() = User(
+        token = token,
+        email = email,
+        password = password,
+        name = name,
+        surname = surname,
+        picture = picture,
+        isAdmin = isAdmin,
+        isFirstLogin = isFirstLogin,
+        lastSeen = lastSeen,
+        dateOfBirth = dateOfBirth,
+        teamId = teamId,
+        roleId = roleId,
+        familyId = familyId,
+        isActive = isActive
+    )
+}
---
+++ app/src/main/java/com/txurdinaga/proyectofinaldam/data/repo/UserRepository.kt
@@ -0,0 +1,56 @@
+package com.txurdinaga.proyectofinaldam.data.repo
+
+import android.util.Log
+import com.txurdinaga.proyectofinaldam.data.model.User
+import com.txurdinaga.proyectofinaldam.data.repo.Constants.API_ENTRY_POINT
+import com.txurdinaga.proyectofinaldam.data.repo.Constants.REGISTER_ROUTE
+import com.txurdinaga.proyectofinaldam.data.repo.Constants.SERVER_URL
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.contentType
+import io.ktor.http.isSuccess
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.json.Json
+
+interface IUserRepository {
+    suspend fun register(user: User)
+}
+private object Constants {
+    const val SERVER_URL = "http://10.0.2.2:8080"
+    const val API_ENTRY_POINT = "/api/v1"
+
+    const val REGISTER_ROUTE = "/user/register"
+}
+class UserRepository() : IUserRepository {
+
+
+    private val client = HttpClient(Android) {
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+            })
+        }
+    }
+
+    override suspend fun register(user: User) {
+        val response: HttpResponse = withContext(Dispatchers.IO) {
+            client.post("$SERVER_URL$API_ENTRY_POINT$REGISTER_ROUTE") {
+                contentType(io.ktor.http.ContentType.Application.Json)
+                setBody(user)
+            }
+        }
+        if (response.status.isSuccess()) {
+            Log.d("USER_REPOSITORY", "REGISTER: SUCCESS")
+        } else {
+            Log.d("USER_REPOSITORY", "REGISTER: ERROR")
+        }
+    }
+
+}
