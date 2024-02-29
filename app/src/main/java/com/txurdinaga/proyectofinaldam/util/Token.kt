358111be766987333290d4b4d1f1d23fcaf652f2
tortilla-seca <tortilla-seca.u0ves@simplelogin.com>
2024-02-29 06:24:22 +0100

    remove token util in favor of shared prefs

--- app/src/main/java/com/txurdinaga/proyectofinaldam/util/Token.kt
+++
@@ -1,7 +0,0 @@
-package com.txurdinaga.proyectofinaldam.util
-
-object Token {
-    fun getToken(): String {
-        return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJBdXRoZW50aWNhdGlvbiIsImlzcyI6InVua2luYS1zZXJ2ZXIiLCJhdWQiOiJ1bmtpbmEtY2xpZW50IiwidXNlcklkIjoiODAzYzZkYjAtODBkZS00ZjI1LThmNmItMDlhMGFiNjBlNzg0IiwiaXNBZG1pbiI6dHJ1ZX0.Sr0N8mhKljpaoN1a_9sNAQvYe6EXPBppGC3k_xAgDoQ"
-    }
-}
