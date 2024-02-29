69105f74203bcef092785a813c4460ca404fd336
Diego Navarro <diegoonavarro2002@gmail.com>
2024-02-29 20:18:09 +0100

    idioma

--- app/src/main/java/com/txurdinaga/proyectofinaldam/ui/MainActivity.kt
+++ app/src/main/java/com/txurdinaga/proyectofinaldam/ui/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.txurdinaga.proyectofinaldam.ui
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
-import android.widget.ImageButton
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -17,14 +15,9 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.txurdinaga.proyectofinaldam.R
-import com.txurdinaga.proyectofinaldam.data.model.Category
-import com.txurdinaga.proyectofinaldam.data.model.UserBuilder
-import com.txurdinaga.proyectofinaldam.data.repo.CategoryRepository
-import com.txurdinaga.proyectofinaldam.data.repo.UserRepository
 import com.txurdinaga.proyectofinaldam.databinding.ActivityMainBinding
 import com.txurdinaga.proyectofinaldam.ui.util.NetworkConnectivityMonitor
-import com.txurdinaga.proyectofinaldam.util.EncryptedPrefsUtil
-import kotlinx.coroutines.launch
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,57 +25,49 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Verificar si es la primera vez que se abre la aplicación
+        sharedPreferences = getSharedPreferences("preferences", Context.MODE_PRIVATE)
+        val isFirstTime = sharedPreferences.getBoolean("first_time", true)
+        if (isFirstTime) {
+            setLocale("eu") // Establecer el idioma en euskera
+            sharedPreferences.edit().putBoolean("first_time", false).apply() // Marcar que ya no es la primera vez
+        } else {
+            // Restaurar el idioma seleccionado por el usuario la última vez
+            val lastLanguage = sharedPreferences.getString("language", "eu") ?: "eu"
+            setLocale(lastLanguage)
+        }
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         drawerLayout = binding.drawerLayout
         initNavigation()
 
-        // Inicialización del las funciones de utilidad de encrypted shared prefs como singleton
-        // Lo inicializamos aquí pasándole el ApplicationContext global para no tener que hacerlo
-        // en los repositorios
-        EncryptedPrefsUtil.init(this.applicationContext)
-
-        supportActionBar?.apply {
-            title="Unkina SBT"
-            setDisplayShowHomeEnabled(true)
-            setDisplayUseLogoEnabled(true)
-            setDisplayHomeAsUpEnabled(true)
-            setCustomView(R.layout.custom_action_bar)
-            setDisplayShowCustomEnabled(true)
-        }
-
-
-
-
         val networkMonitor = NetworkConnectivityMonitor(this)
-        networkMonitor.observeNetworkConnectivity(this
-        ) { isConnected ->
+        networkMonitor.observeNetworkConnectivity(this) { isConnected ->
             if (!isConnected) {
                 Toast.makeText(this, "Conexión a internet perdida", Toast.LENGTH_SHORT).show()
             }
         }
+    }
 
-
-
-
-
-        // Encuentra la imagen por su ID
-        var unkinaLogoImageView = findViewById<ImageView>(R.id.unkina_logo)
-
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
-        val navController = navHostFragment.navController
-
-        unkinaLogoImageView.setOnClickListener {
-            navController.navigate(R.id.principalFragment)
-        }
+    private fun setLocale(language: String) {
+        val locale = Locale(language)
+        Locale.setDefault(locale)
+        val configuration = resources.configuration
+        configuration.setLocale(locale)
+        resources.updateConfiguration(configuration, resources.displayMetrics)
+        // Guardar el último idioma seleccionado
+        sharedPreferences.edit().putString("language", language).apply()
     }
 
     private fun initNavigation() {
         val navHost = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         navController = navHost.navController
-        // Aquí especificamos los ID de los destinos de nivel superior, excluyendo el ID del destino de inicio
+        // Especifica los ID de los destinos de nivel superior, excluyendo el ID del destino de inicio
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.principalFragment,
@@ -99,14 +84,13 @@ class MainActivity : AppCompatActivity() {
                 R.id.galeriaFragment,
                 R.id.gestionPDFs,
                 R.id.gestionOcupacionesFragment,
-                R.id.personalFragment
-
+                R.id.personalFragment,
+                R.id.ajustesFragment2
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         binding.navView.setupWithNavController(navController)
 
-        // Escuchar cambios en los destinos del NavController
         navController.addOnDestinationChangedListener { controller, destination, _ ->
             if (destination.id == R.id.principalFragment ||
                 destination.id == R.id.gestionEquiposFragment ||
@@ -122,13 +106,13 @@ class MainActivity : AppCompatActivity() {
                 destination.id == R.id.galeriaFragment ||
                 destination.id == R.id.gestionPDFs ||
                 destination.id == R.id.gestionOcupacionesFragment ||
-                destination.id == R.id.personalFragment
+                destination.id == R.id.personalFragment ||
+                destination.id == R.id.ajustesFragment2
             ) {
-                supportActionBar?.title = "Unkina SBT" // Establecer el título deseado para los destinos específicos
+                supportActionBar?.title = "Unkina SBT"
             }
         }
     }
-
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.navHostFragment)
@@ -138,5 +122,4 @@ class MainActivity : AppCompatActivity() {
         }
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
-
 }
