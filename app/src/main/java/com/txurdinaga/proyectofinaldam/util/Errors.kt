620ba112b2302fe1d69cb65ef99bbf5ba15d1a64
tortilla-seca <tortilla-seca.u0ves@simplelogin.com>
2024-02-29 06:24:35 +0100

    add delete error case

--- app/src/main/java/com/txurdinaga/proyectofinaldam/util/Errors.kt
+++ app/src/main/java/com/txurdinaga/proyectofinaldam/util/Errors.kt
@@ -4,5 +4,5 @@ class LoginError : RuntimeException()
 class RegisterError : RuntimeException()
 class UpdateError : RuntimeException()
 class CreateError : RuntimeException()
-
+class DeleteError : RuntimeException()
 class GetAllError : RuntimeException()
