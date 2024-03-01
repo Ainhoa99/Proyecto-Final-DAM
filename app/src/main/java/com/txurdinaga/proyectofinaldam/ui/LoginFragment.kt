9fe823ce95388342ed998b7796c51138e83db12f
JdanielRo <josedanieltxurdinaga@gmail.com>
2024-03-01 02:56:32 +0100

    login funcionamiento correcto

--- app/src/main/java/com/txurdinaga/proyectofinaldam/ui/LoginFragment.kt
+++ app/src/main/java/com/txurdinaga/proyectofinaldam/ui/LoginFragment.kt
@@ -6,8 +6,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.txurdinaga.proyectofinaldam.R
 import com.txurdinaga.proyectofinaldam.data.repo.UserRepository
 import com.txurdinaga.proyectofinaldam.databinding.FragmentLoginBinding
 import com.txurdinaga.proyectofinaldam.util.CreateError
@@ -31,7 +34,9 @@ class LoginFragment : Fragment() {
 
                     EncryptedPrefsUtil.putString("tokenLogin",userRepo.login(binding.email.text.toString(),binding.password.text.toString()))
                     EncryptedPrefsUtil.putBoolean("isAdmin",userRepo.isAdmin())
-
+                    activity?.recreate()
+                    findNavController().navigate(R.id.action_loginFragment2_to_principalFragment)
+                    Toast.makeText(context,"correcto",Toast.LENGTH_LONG).show()
                 } catch(loginE: LoginError) {
                     // Mostrar mensaje de error sobre problemas con la autenticación o permisos
                 }
